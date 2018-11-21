820ccc18efd256fe695eef8588aac16bbba24f69
Yauhen <manuila.yauhen@gmail.com>
2018-11-21 17:07:31 +0300

    add react.webpack.config

---
+++ react.webpack.config/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+const App = () => (
+  <div className="app">
+    <h1>Hello</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
